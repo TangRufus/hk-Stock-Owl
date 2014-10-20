ba46c4a6242fd3af486306ffecb91cf9c28b9694
Tang Rufus <tangrufus@gmail.com>
2014-10-21 07:23:17 +0800

    ActiveAdmin: Add Stock Company Ex Documents one-to-many relationship

--- app/admin/ex_document.rb
+++ app/admin/ex_document.rb
@@ -2,6 +2,9 @@ ActiveAdmin.register ExDocument do
 
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  permit_params :title, :link, :stock_company_id
+  permit_params :title, :link
+
+  belongs_to :stock_company
+  navigation_menu :stock_company
 
 end
