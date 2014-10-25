7e5a8f89e42eb43be4d42f4f573ed845ca3c46a8
Tang Rufus <tangrufus@gmail.com>
2014-10-25 15:05:34 +0800

    ExDocumentBuilder: Typo

--- app/workers/ex_document_builder.rb
+++ app/workers/ex_document_builder.rb
@@ -1,5 +1,5 @@
 class ExDocumentBuilder
-  @queue = :hk_ex_news_fetcher
+  @queue = :ex_document_builder
 
   def self.perform(hkt_released_at, stock_code, stock_name, headline_categories, title, link)
     ExDocument.find_or_create_from_hkexnews(hkt_released_at, stock_code, stock_name, headline_categories, title, link)
