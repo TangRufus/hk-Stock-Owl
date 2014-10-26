250ed7c4cb3f81c4004d744099928d4be53a9f39
Tang Rufus <tangrufus@gmail.com>
2014-10-27 07:52:40 +0800

    ExDocument: Early quit if StockCompany is not valid

--- app/models/ex_document.rb
+++ app/models/ex_document.rb
@@ -37,12 +37,14 @@ class ExDocument < ActiveRecord::Base
 
     sc = StockCompany.find_or_create_from_hkexnews stock_code, stock_name
 
+    return nil if sc.nil?
+
     title = title.gsub(/\n/, ' ').gsub(/\r/, ' ').titleize.squeeze(" ").strip
     link = link.gsub(/\n/, '').gsub(/\r/, '').gsub(' ', '')
 
     doc = ExDocument.where(:released_at => released_at, :stock_company_id => sc.id, :link => link).first
 
-    if doc.nil? && sc.present?
+    if doc.nil?
       doc = sc.ex_documents.create :released_at => released_at, :title => title, :link => link
       cats = ExHeadlineCategory.find_or_create_from_hkexnews headline_categories
       doc.ex_headline_categories = cats
