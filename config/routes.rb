fee07c3df77f5576ca4b4fcde5085508f8259504
Tang Rufus <tangrufus@gmail.com>
2014-10-27 07:41:13 +0800

    Devise: Gradual engagement, email only sign up

--- config/routes.rb
+++ config/routes.rb
@@ -71,11 +71,12 @@
 
 Rails.application.routes.draw do
 
-  devise_for :users, :skip => [:sessions]
+  devise_for :users, :skip => [:sessions], :controllers => {:confirmations => 'confirmations'}
   as :user do
     get 'login' => 'devise/sessions#new', :as => :new_user_session
     post 'login' => 'devise/sessions#create', :as => :user_session
     delete 'logout' => 'devise/sessions#destroy', :as => :destroy_user_session
+    patch 'confirm' => "confirmations#confirm"
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
