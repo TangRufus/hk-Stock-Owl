ee77f9ce91706dcf864944c390f3a8c56ba73ea6
Tang Rufus <tangrufus@gmail.com>
2014-11-07 08:42:42 +0800

    SubscriptionsController: Define create

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,20 +1,39 @@
 class SubscriptionsController < ApplicationController
   def create
+    sc = StockCompany.where(subscription_params).first
+    if sc.nil?
+      redirect_to subscriptions_path, :flash => { :error => 'Stock company not found' }
+      return
+    end
+
+    if current_user.target_companies.include?(sc)
+      redirect_to subscriptions_path, :flash => { :error => 'You have subscribed' }
+      return
+    end
+
+    flash[:notice] = 'Stock company added' if current_user.target_companies << sc
+
+    redirect_to subscriptions_path
   end
 
   def destroy
     sub = Subscription.destroy(params[:id])
     if sub.user == current_user && sub.destroy
-        flash[:notice] = 'Subscription canncelled'
-      else
-        flash[:error] = 'Somethings wrong..'
-      end
+      flash[:notice] = 'Subscription canncelled'
+    else
+      flash[:error] = 'Somethings wrong..'
+    end
 
     redirect_to subscriptions_path
   end
 
   def index
     @subscriptions = current_user.subscriptions
+  end
+
+  private
+  def subscription_params
+    params.require(:subscription).permit(:code)
   end
 
 end
